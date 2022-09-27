4c2235bd5edec1dcb7060044e24151cf2f89eb2c
okquentin <qosterhage@outlook.com>
2022-09-27 17:52:10 -0400

    P5-3 Complete

--- p5-3.source.cpp
+++ p5-3.source.cpp
@@ -15,26 +15,31 @@ int number = 0;
     int digit7;
     int digit8;
 
+bool quit = false;
 
 // User Prompt
 
-    cout << "Please enter a number: ";
+for(;;){
+   cout << "Please enter a whole number: ";
         cin >> number;
+    if (number < 0) // Failed input if not a whole number
+    cout << "Be sure to enter a whole number." << endl; 
+    if (number == 0) // Dealing with 'zero' input
+    cout << "Number in binary: " << 0;
+    if (number > 0)
+    break;
+}
+ 
 
 // Algorithim
-
-if(number == 0){
-    cout << 0;
-}
-
+  
 while(number >0){
-
 
 digit1 = number % 2;
     number /= 2; // Storing quotient
 digit2 = number % 2;
 	number /= 2;
-	
+
 digit3 = number % 2;
     number /= 2; 
 digit4 = number % 2;
@@ -52,20 +57,26 @@ digit8 = number % 2;
 	
 }
 
-// cout << digit1 << endl;
-// cout << digit2 << endl;
-// cout << digit3 << endl;
-// cout << digit4 << endl;
-// cout << digit5 << endl;
-// cout << digit6 << endl;
-// cout << digit7 << endl;
-// cout << digit8 << endl;
+// Converting output to a one-line string
 
-// Output
+string string1 = to_string(digit1);
+string string2 = to_string(digit2);
+string string3 = to_string(digit3);
+string string4 = to_string(digit4);
+string string5 = to_string(digit5);
+string string6 = to_string(digit6);
+string string7 = to_string(digit7);
+string string8 = to_string(digit8);
 
-if(digit1 != 0){
-    cout << digit1 << endl;
-}
+string binaryString = string8 + string7 + string6 + string5 +string4 + string3 + string2 + string1;
+
+// Removing leading zeros by converting the string into an integer value
+
+int binaryOut = stoi(binaryString);
+
+// Outputting the binary number result
+
+cout << binaryOut << endl;
 
 return 0;
 } 
