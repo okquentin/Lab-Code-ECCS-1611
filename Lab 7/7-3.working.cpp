a07d30e2c189497289788b710faf6ba0825fbcb5
okquentin <112091207+okquentin@users.noreply.github.com>
2022-10-16 19:35:41 -0400

    Update 7-3.working.cpp

--- Lab 7/7-3.working.cpp
+++ Lab 7/7-3.working.cpp
@@ -18,11 +18,7 @@ int main()
     if (!cin.fail())
     {
         cout << "First digit: " << first_digit(n) << " Last digit: " << last_digit(n) << " Number of digits: " << digits(n);
-    } else{
-        cout << "Please try entering digits again." << endl;
-        continue;
     }
-
 return 0;
 }
 
