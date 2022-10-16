96940c498ca48bb745a5af1b0a89fa9d64533e9c
okquentin <112091207+okquentin@users.noreply.github.com>
2022-10-16 19:31:02 -0400

    Update 7-3.working.cpp

--- Lab 7/7-3.working.cpp
+++ Lab 7/7-3.working.cpp
@@ -3,33 +3,8 @@
 using namespace std;
 
 int first_digit(int n);
-int first_digit(int n)
-{
-    while (n >= 10)
-    {
-        n = n / 10;
-    }
-    return (n);
-}
-
 int last_digit(int n);
-int last_digit(int n)
-{
-    n = n % 10;
-    return (n);
-}
-
 int digits(int n);
-int digits(int n)
-{
-    int i = 0;
-    while (n > 0)
-    {
-        n = n / 10;
-        i++;
-    }
-    return (i);
-}
 
 int main()
 {
@@ -39,13 +14,34 @@ int main()
     cout << "Enter number or Q to quit: ";
     cin >> n;
 
-    if (cin.fail())
-    {
-        cout << "No data.";
-    }
-    else
+    if (!cin.fail())
     {
         cout << "First digit: " << first_digit(n) << " Last digit: " << last_digit(n) << " Number of digits: " << digits(n);
     }
 
+}
+int first_digit(int n)
+{
+    while (n >= 10)
+    {
+        n = n / 10;
+    }
+    return (n);
+}
+
+int last_digit(int n)
+{
+    n = n % 10;
+    return (n);
+}
+
+int digits(int n)
+{
+    int i = 0;
+    while (n > 0)
+    {
+        n = n / 10;
+        i++;
+    }
+    return (i);
 }
