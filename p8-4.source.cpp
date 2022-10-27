08ffdd0a805bab1278d9bfd8eb54bd4ba3b97ec6
okquentin <qosterhage@outlook.com>
2022-10-27 10:07:00 -0400

    Lab 8 Komplett

--- p8-4.source.cpp
+++ p8-4.source.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-// *** Function Prototypes ***
 int romanCharValue(char r);
+
 int convertRomanToInt(string s);
 
-int main(){
-
-
-
-// END OF MAIN
-return 0;
+int main()
+{
+    while (true)
+    {
+        string s;
+        cout << "Enter Roman number or Q to quit: ";
+        cin >> s;
+        if (s == "q" || s == "Q")
+        {
+            break;
+        }
+        cout << s << " = " << convertRomanToInt(s) << endl;
+    }
 }
 
-// ** Function Headers ***
-int romanCharValue(char r){
-
+int convertRomanToInt(string s)
+{
+    int total = 0;
+    while (s.length() > 0)
+    {
+        if (romanCharValue(s[0]) >= romanCharValue(s[1]))
+        {
+            total += romanCharValue(s[0]);
+            s.erase(0, 1);
+        }
+        else
+        {
+            total += (romanCharValue(s[1]) - romanCharValue(s[0]));
+            s.erase(0, 1);
+            s.erase(0, 1);
+        }
+    }
+    return total;
 }
 
-int convertRomanToInt(string s){
-
+int romanCharValue(char r)
+{
+    switch (r)
+    {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default: return 0;
+    }
 }
