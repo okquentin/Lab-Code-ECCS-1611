f9bfb14db25aa201052d4b1ae982e33bafd20435
okquentin <qosterhage@outlook.com>
2022-10-26 23:22:01 -0400

    Update p8-4.source.cpp

---
+++ p8-4.source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// *** Function Prototypes ***
+int countWords(string str);
+
+int main(){
+
+
+
+// END OF MAIN
+return 0;
+}
+
+// ** Function Headers ***
